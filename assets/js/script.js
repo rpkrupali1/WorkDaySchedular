5285034719301fe33f1993f35e79da168d1eb3ea
Krupali Pilgulwar <rpkrupali@gmail.com>
2022-01-02 08:19:02 -0600

    logic for storing in local and load web page from local storage

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,10 +1,15 @@
+var events = {};
+// display current day using moment
 $("#currentDay").text(moment().format("dddd, MMMM Do"));
 
+//This function is to add colors to description of each hour after comaring with current hour
 var hourChecker = function(){
+    //get current hour using moment
     var currentHour = moment().hour();
+
+    //loop through every hour in calendar block and add cllases after comparing with current hour
     $(".time-block").each(function(){
-        var timeBlock = parseInt($(this).attr("id"));
-        console.log(timeBlock);
+        var timeBlock = parseInt($(this).attr("id")); //get id as id's are given for each hour
         if(timeBlock < currentHour){
             $(this).addClass("past");
         }
@@ -18,7 +23,36 @@ var hourChecker = function(){
             $(this).addClass("future");
         }
     });
+};
+
+
+//save events in local storage
+var loadEvent = function(id,text){
+    events = JSON.parse(window.localStorage.getItem("events")) || {}
+    $.each(events,function(hour,description){
+        $('#' + hour).find("textarea").val(description);
+    })
     
 }
 
+var saveEvent = function() {
+    localStorage.setItem("events", JSON.stringify(events));
+  };
+
+var createEvent = function(id, text){
+    events[id] = text;
+    saveEvent();
+}
+
+//check for hour and color code acordingly
 hourChecker();
+loadEvent();
+
+//when save button is clicked
+$(".saveBtn").click(function(){
+    var parenEl = $(this).parent();
+    var id = $(parenEl).attr("id"); //get current hour
+    var text = $(parenEl).find("textarea").val().trim(); //get text    
+    createEvent(id,text);
+});
+
